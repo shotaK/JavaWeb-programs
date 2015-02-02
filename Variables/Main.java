434d0ab22c652e4e8f42fc8d67d15b13c1fcd6cd
Shota <sh.karkashadze@gmail.com>
2015-02-02 17:12:40 +0200

    add Variables program

---
+++ Variables/Main.java
@@ -0,0 +1,36 @@
+package com.javaweb;
+
+public class Main {
+    /*--------------Declaring class variables-----------------*/
+    static String myString;
+
+    /*--------------Final Variables (Constants)-----------------*/
+    static final int SEASONS = 12;
+
+    public static void main(String[] args) {
+
+        /*--------------Declaring Variables-----------------*/
+        int myInt;
+        String fullname;
+        double radius;
+
+        /*--------------Declaring two or more variables in one statement-----------------*/
+        int x, y, z;
+        String firstname, lastname, gender;
+
+        /*--------------Declaring class variables-----------------*/
+        myString = "This is String data";
+        System.out.print(myString);
+
+        /*--------------Initializing Variables-----------------*/
+        int x2;
+        x = 15;
+        System.out.print(x);
+
+        /*--------------Initializing variables with initializers-----------------*/
+        int x3 = 15;
+        String firstname3 = "Curtis";
+        double radius3 = 23.5;
+        System.out.print(x);
+    }
+}
